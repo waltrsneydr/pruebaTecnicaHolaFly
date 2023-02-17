2df55c82eea72f50f7e86c9fc0c78dd2379cd602
Walter Esneyder Barbosa Castañeda - Ceiba Software <walter.barbosa@ceiba.com.co>
2023-02-16 23:38:00 -0500

    first commit

---
+++ src/server/endpoints/swapiEndpoints.js
@@ -0,0 +1,36 @@
+
+const _isWookieeFormat = (req) => {
+    if(req.query.format && req.query.format == 'wookiee'){
+        return true;
+    }
+    return false;
+}
+
+
+const applySwapiEndpoints = (server, app) => {
+
+    server.get('/hfswapi/test', async (req, res) => {
+        const data = await app.swapiFunctions.genericRequest('https://swapi.dev/api/', 'GET', null, true);
+        res.send(data);
+    });
+
+    server.get('/hfswapi/getPeople/:id', async (req, res) => {
+        res.sendStatus(501);
+    });
+
+    server.get('/hfswapi/getPlanet/:id', async (req, res) => {
+        res.sendStatus(501);
+    });
+
+    server.get('/hfswapi/getWeightOnPlanetRandom', async (req, res) => {
+        res.sendStatus(501);
+    });
+
+    server.get('/hfswapi/getLogs',async (req, res) => {
+        const data = await app.db.logging.findAll();
+        res.send(data);
+    });
+
+}
+
+module.exports = applySwapiEndpoints;
